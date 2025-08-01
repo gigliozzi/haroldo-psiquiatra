ed6626410336449f96e8813b9e476f31b77f395d
William Gigliozzi <gigliozzi@gmail.com>
2025-08-01 14:32:14 -0300

    Configuração de variável de ambiente para Formspree

--- src/app/components/Contato.tsx
+++ src/app/components/Contato.tsx
@@ -4,12 +4,14 @@ import { FaPhoneAlt, FaEnvelope, FaWhatsapp } from "react-icons/fa";
 import { useForm, ValidationError } from "@formspree/react";
 
 export default function Contato() {
-  const [state, handleSubmit] = useForm("xzzvvlvr"); // <-- seu ID do Formspree
+  const [state, handleSubmit] = useForm(
+    process.env.NEXT_PUBLIC_FORMSPREE_ID as string
+  );
+  // <-- seu ID do Formspree
 
   return (
     <section id="contato" className="py-20 bg-white">
       <div className="container mx-auto px-6 grid md:grid-cols-2 gap-12">
-        
         {/* Formulário */}
         <motion.div
           initial={{ opacity: 0, x: -50 }}
@@ -17,7 +19,9 @@ export default function Contato() {
           viewport={{ once: true }}
           transition={{ duration: 0.8 }}
         >
-          <h2 className="text-3xl font-bold text-gray-900 mb-6">Fale Conosco</h2>
+          <h2 className="text-3xl font-bold text-gray-900 mb-6">
+            Fale Conosco
+          </h2>
           <p className="text-lg text-gray-600 mb-6">
             Entre em contato para agendar sua consulta ou tirar dúvidas.
           </p>
@@ -43,8 +47,12 @@ export default function Contato() {
                 required
                 className="w-full border border-gray-300 rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus:ring-sky-400"
               />
-              <ValidationError prefix="Email" field="email" errors={state.errors} />
-              
+              <ValidationError
+                prefix="Email"
+                field="email"
+                errors={state.errors}
+              />
+
               <textarea
                 id="message"
                 name="message"
@@ -53,7 +61,11 @@ export default function Contato() {
                 required
                 className="w-full border border-gray-300 rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus:ring-sky-400"
               ></textarea>
-              <ValidationError prefix="Message" field="message" errors={state.errors} />
+              <ValidationError
+                prefix="Message"
+                field="message"
+                errors={state.errors}
+              />
 
               <button
                 type="submit"
@@ -73,25 +85,27 @@ export default function Contato() {
           viewport={{ once: true }}
           transition={{ duration: 0.8 }}
         >
-          <h3 className="text-2xl font-semibold text-gray-800 mb-4">Informações de Contato</h3>
+          <h3 className="text-2xl font-semibold text-gray-800 mb-4">
+            Informações de Contato
+          </h3>
           <ul className="space-y-4 mb-8">
             <li className="flex items-center gap-3 text-gray-700">
-              <FaPhoneAlt className="text-sky-600" /> 
+              <FaPhoneAlt className="text-sky-600" />
               <span suppressHydrationWarning>(62) 99999-9999</span>
             </li>
             <li className="flex items-center gap-3 text-gray-700">
-              <FaWhatsapp className="text-sky-600" /> 
+              <FaWhatsapp className="text-sky-600" />
               <span suppressHydrationWarning>WhatsApp: (62) 99999-9999</span>
             </li>
             <li className="flex items-center gap-3 text-gray-700">
-              <FaEnvelope className="text-sky-600" /> 
-               <span suppressHydrationWarning>contato@clinica.com</span>
+              <FaEnvelope className="text-sky-600" />
+              <span suppressHydrationWarning>contato@clinica.com</span>
             </li>
           </ul>
 
           <div className="rounded-lg overflow-hidden shadow-lg">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3821.954111524142!2d-49.2506439!3d-16.679179599999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935ef17ba52023f9%3A0xf9e4ae3b007c7ed7!2sR.%2010%2C%20420%20-%20St.%20Sul%2C%20Goi%C3%A2nia%20-%20GO%2C%2074080-420!5e0!3m2!1spt-BR!2sbr!4v1754052349742!5m2!1spt-BR!2sbr" 
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3821.954111524142!2d-49.2506439!3d-16.679179599999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935ef17ba52023f9%3A0xf9e4ae3b007c7ed7!2sR.%2010%2C%20420%20-%20St.%20Sul%2C%20Goi%C3%A2nia%20-%20GO%2C%2074080-420!5e0!3m2!1spt-BR!2sbr!4v1754052349742!5m2!1spt-BR!2sbr"
               width="100%"
               height="250"
               style={{ border: 0 }}
